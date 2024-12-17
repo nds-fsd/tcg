127788ff860b3a503021ada981d49f1dfc09b5df
Edgar Chiquillo <edgarchb44@gmail.com>
2024-12-17 14:58:01 +0100

    Entity of userController finished

---
+++ backend/src/data/schemas/userCollection.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require('mongoose');
+
+const userCollectionSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+    },
+    cardId: {
+        type: Schema.Types.ObjectId,
+        ref: "card",
+    }
+});
+
+const UserCollection = model('user', userCollectionSchema);
+
+
+module.exports = UserCollection;
