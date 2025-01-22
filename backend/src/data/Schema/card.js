f8d07469593fb57dd85d1c749be7908e5b638da1
Amaia Bordas <abordasc@hotmail.com>
2025-01-22 15:25:25 +0100

    Add JWT middleware for Card and Deck controllers and routes

--- backend/src/data/Schema/card.js
+++ backend/src/data/Schema/card.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require('mongoose');
 
+
 const cardSchema = new Schema(
   {
     name: { type: String, required: true },
--- backend/src/routers/cardRouter.js
+++ backend/src/routers/cardRouter.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
 const { getCards, getCardById, createCard, updateCard, deleteCard } = require('../controllers/cardController');
+const { jwtMiddleware } = require('../security/jwt');
 
 const cardRouter = Router();
 
 cardRouter.get('/', getCards);
-cardRouter.post('/', createCard);
+cardRouter.post('/', jwtMiddleware, createCard);
 cardRouter.get('/:id', getCardById);
-cardRouter.put('/:id', updateCard);
-cardRouter.delete('/:id', deleteCard);
+cardRouter.put('/:id', jwtMiddleware, updateCard);
+cardRouter.delete('/:id', jwtMiddleware, deleteCard);
 
 module.exports = { cardRouter };
