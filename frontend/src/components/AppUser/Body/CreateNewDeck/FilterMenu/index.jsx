009e7746c18337734655f4445eaf795be28aa9c4
Amaia Bordas <abordasc@hotmail.com>
2025-01-26 22:37:49 +0100

    Fix some code and styles for FilterMenu, Sort, Displays and Title

--- frontend/src/components/AppUser/Body/CreateNewDeck/FilterMenu/index.jsx
+++ frontend/src/components/AppUser/Body/CreateNewDeck/FilterMenu/index.jsx
@@ -2,67 +2,99 @@ import React from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
 import styles from './filtermenu.module.css';
 
+const attributeTranslations = {
+  water: 'Agua',
+  fire: 'Fuego',
+  darkness: 'Oscuridad',
+  light: 'Luz',
+  earth: 'Tierra',
+  wind: 'Viento',
+};
+
 const FilterMenu = ({ filters, onFilterChange, onClearFilters }) => {
-  const categories = ['Monster', 'Support', 'Fusion'];
-  const types = ['Beast', 'Warrior', 'Demon', 'Fairy', 'Zombie', 'Plant'];
-  const attributes = ['Water', 'Fire', 'Darkness', 'Light', 'Earth', 'Wind'];
-  const rarities = ['Common', 'Rare', 'Epic', 'Legendary'];
+  const categories = ['Monstruo', 'Apoyo', 'Fusión'];
+  const types = [
+    'Bestia',
+    'Guerrero',
+    'Demonio',
+    'Hada',
+    'Zombie',
+    'Planta',
+    'Máquina',
+    'Insecto',
+    'Dragón',
+    'Pez',
+    'Roca',
+    'Normal',
+    'Rápida',
+    'Equipo',
+    'Continua',
+    'Contraefecto',
+  ];
+  const attributes = ['water', 'fire', 'darkness', 'light', 'earth', 'wind'];
+  const rarities = ['Común', 'Rara', 'Épica', 'Legendaria'];
 
   return (
     <div className={styles.filterMenu}>
-      <h4>Filtros</h4>
       <div className={styles.filterSection}>
         <h5>Categoría</h5>
-        {categories.map((category) => (
-          <label key={category}>
-            <input
-              type='checkbox'
-              checked={filters.category.includes(category)}
-              onChange={() => onFilterChange('category', category)}
-            />
-            {category}
-          </label>
-        ))}
+        <select
+          value={filters.category}
+          onChange={(e) => onFilterChange('category', e.target.value)}
+          className={styles.select}
+        >
+          <option value=''>Categorías</option>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
       </div>
+
       <div className={styles.filterSection}>
         <h5>Tipo</h5>
-        {types.map((type) => (
-          <label key={type}>
-            <input
-              type='checkbox'
-              checked={filters.type.includes(type)}
-              onChange={() => onFilterChange('type', type)}
-            />
-            {type}
-          </label>
-        ))}
+        <select value={filters.type} onChange={(e) => onFilterChange('type', e.target.value)} className={styles.select}>
+          <option value=''>Tipos</option>
+          {types.map((type) => (
+            <option key={type} value={type}>
+              {type}
+            </option>
+          ))}
+        </select>
       </div>
+
       <div className={styles.filterSection}>
         <h5>Atributo</h5>
-        {attributes.map((attribute) => (
-          <label key={attribute}>
-            <input
-              type='checkbox'
-              checked={filters.type.includes(attribute)}
-              onChange={() => onFilterChange('attribute', attribute)}
-            />
-            {attribute}
-          </label>
-        ))}
+        <select
+          value={filters.attribute}
+          onChange={(e) => onFilterChange('attribute', e.target.value)}
+          className={styles.select}
+        >
+          <option value=''>Atributos</option>
+          {Object.entries(attributeTranslations).map(([key, value]) => (
+            <option key={key} value={key}>
+              {value}
+            </option>
+          ))}
+        </select>
       </div>
       <div className={styles.filterSection}>
         <h5>Rareza</h5>
-        {rarities.map((rarity) => (
-          <label key={rarity}>
-            <input
-              type='checkbox'
-              checked={filters.rarity.includes(rarity)}
-              onChange={() => onFilterChange('rarity', rarity)}
-            />
-            {rarity}
-          </label>
-        ))}
+        <select
+          value={filters.rarity}
+          onChange={(e) => onFilterChange('rarity', e.target.value)}
+          className={styles.select}
+        >
+          <option value=''>Rarezas</option>
+          {rarities.map((rarity) => (
+            <option key={rarity} value={rarity}>
+              {rarity}
+            </option>
+          ))}
+        </select>
       </div>
+
       <button className={styles.clearButton} onClick={onClearFilters}>
         <FaTrashAlt className={styles.icon} /> Limpiar Filtros
       </button>
