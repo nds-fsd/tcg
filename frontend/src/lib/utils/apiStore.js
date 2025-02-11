32d1538e0c08ae0a8988afe2680cd2b9030f7991
Amaia Bordas <abordasc@hotmail.com>
2025-02-11 21:53:34 +0100

    Add getUserToken in apiStore

--- frontend/src/lib/utils/apiStore.js
+++ frontend/src/lib/utils/apiStore.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { getUserToken } from './localStorage.utils';
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_API_URL,
@@ -7,42 +8,53 @@ const API = axios.create({
 export const getProducts = async () => {
   try {
     const response = await API.get("/store");
-    console.log('Resposta', response.data);
     return response.data;
   } catch (error) {
-    console.error("❌ Error al obtener productos:", error);
+    console.error("Error al obtener productos:", error);
     return [];
   }
 };
 
-export const buyChest = async (productId, token) => {
+export const buyChest = async (productId) => {
+  const token = getUserToken();
+
+  if (!token) {
+    console.error("Error: Usuario no autenticado.");
+    return null;
+  }
+  
   try {
     const response = await API.post(`/buy/chest/${productId}`, {}, { 
       headers: { 
         Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json"
       },
     });
 
     return response.data;
   } catch (error) {
-    console.error("❌ Error al comprar el cofre:", error.response ? error.response.data : error);
+    console.error("Error al comprar el cofre:", error);
     return null;
   }
 };
 
-export const buyCurrency = async (productId, token) => {
+export const buyCurrency = async (productId) => {
+  const token = getUserToken();
+
+  if (!token) {
+    console.error("Error: Usuario no autenticado.");
+    return null;
+  }
+  
   try {
     const response = await API.post(`/buy/currency/${productId}`, {}, { 
       headers: { 
         Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json"
       },
     });
 
     return response.data;
   } catch (error) {
-    console.error("❌ Error al comprar el pack de Pixelcoins:", error.response ? error.response.data : error);
+    console.error("Error al comprar el pack de Pixelcoins:", error);
     return null;
   }
 };
