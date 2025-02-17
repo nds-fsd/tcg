a25c4dfd7a00616508efe9c9436901b93f24d9f5
Amaia Bordas <abordasc@hotmail.com>
2025-02-17 14:36:09 +0100

    Fix some text messages from HomePage

--- frontend/src/components/AppUser/Body/HomePage/index.jsx
+++ frontend/src/components/AppUser/Body/HomePage/index.jsx
@@ -4,12 +4,12 @@ import styles from "./homePage.module.css";
 const HomePage = () => {
     return (
         <div className={styles.homePagecontainer}>
-            <h1 className={styles.homePageTitle}>Bienvenido a Pixel-Quest-TCG</h1>
+            <h1 className={styles.homePageTitle}>Te damos la bienvenida a PixelQuest TCG</h1>
 
             <p className={styles.homePageDescription}>
-                Pixel-Quest-TCG es un emocionante juego de cartas de estilo medieval fantástico,
-                donde podrás hacer amigos, enfrentarte en batallas épicas y coleccionar poderosas cartas.
-                ¿Estás listo para la aventura?
+                PixelQuest TCG es un emocionante juego de cartas de estilo medieval fantástico,
+                donde podrás hacer amistades, enfrentarte en batallas épicas y coleccionar poderosas cartas.
+                ¿Todo a punto para la aventura?
             </p>
 
             <div className={styles.homePageSections}>
@@ -17,20 +17,20 @@ const HomePage = () => {
                 <div className={styles.homePageSection}>
                     <h2 className={styles.homePageSectionTitle}>¡Desafía a tus rivales!</h2>
                     <p>
-                        Ingresa a la arena y demuestra tu habilidad en batallas estratégicas contra otros jugadores.
+                        Ingresa a la arena y demuestra tu habilidad en batallas estratégicas contra rivales dignos de tu habilidad.
                     </p>
                     <Link to="/battles" className={`${styles.homePageButton} ${styles.battleButton}`}>
-                        Ir a Batallas
+                        Ir a batallas
                     </Link>
                 </div>
 
                 <div className={styles.homePageSection}>
                     <h2 className={styles.homePageSectionTitle}>Conoce al equipo</h2>
                     <p>
-                        Descubre quiénes están detrás de este mundo de fantasía y cómo trabajamos para hacer crecer Pixel-Quest-TCG.
+                        Descubre quiénes están detrás de este mundo de fantasía y cómo trabajamos para hacer crecer PixelQuest TCG.
                     </p>
                     <Link to="/about" className={`${styles.homePageButton} ${styles.aboutButton}`}>
-                        Sobre Nosotros
+                        Sobre nosotros
                     </Link>
                 </div>
             </div>
