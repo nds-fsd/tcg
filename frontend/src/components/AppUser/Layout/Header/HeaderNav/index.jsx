35c05dc58b814256fae6ad077d528910a730bd18
Edgar <edgarchb44@gmail.com>
2025-03-14 10:31:41 +0100

    Marcado de pagina activa en navar

--- frontend/src/components/AppUser/Layout/Header/HeaderNav/index.jsx
+++ frontend/src/components/AppUser/Layout/Header/HeaderNav/index.jsx
@@ -1,20 +1,25 @@
 import styles from './headerNav.module.css';
 import HeaderNavAdmin from './HeaderNavAdmin';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const HeaderNav = () => {
+  const location = useLocation();
+
   return (
     <nav className={styles.nav}>
-      <Link to='/deck' className={styles.navLink}>
+      <Link to='/deck' className={`${styles.navLink} ${location.pathname === '/deck' ? styles.navLinkactive : ''}`}>
         Mazos
       </Link>
-      <Link to='/collection' className={styles.navLink}>
+      <Link
+        to='/collection'
+        className={`${styles.navLink} ${location.pathname === '/collection' ? styles.navLinkactive : ''}`}
+      >
         Colección
       </Link>
-      <Link to='/store' className={styles.navLink}>
+      <Link to='/store' className={`${styles.navLink} ${location.pathname === '/store' ? styles.navLinkactive : ''}`}>
         Tienda
       </Link>
-      <Link to='/market' className={styles.navLink}>
+      <Link to='/market' className={`${styles.navLink} ${location.pathname === '/market' ? styles.navLinkactive : ''}`}>
         Mercado Libre
       </Link>
 
